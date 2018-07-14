0c7bb17b107a911b12d8f786b70365d4c9a34d3a
Dmytro <dmytro@proisp.no>
2018-07-14 12:47:27 +0200

    heroku

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://chevil:7753191lbvf@ds161620.mlab.com:61620/online_os_dev",
+};
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
 const app = express();
 
-mongoose.connect('mongodb://chevil:7753191lbvf@ds161620.mlab.com:61620/online_os_dev');
+mongoose.connect(keys.mongoURI);
 require('./models/DesktopModel');
 
 require('./routes/desktopRoutes')(app);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
 
+    const path = require('path');
+    app.get('*', (req,res)=>{
+        res.sendfile(path.resolve(__dirname,'client', 'build', 'index.html'))
+    });
+}
 
 app.listen(5000);
